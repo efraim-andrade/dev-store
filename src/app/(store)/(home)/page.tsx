a527c89e2a65161a2726724bdee402df2619cd58
Efraim Andrade <efraim.dev@gmail.com>
2024-03-06 22:56:21 -0300

    feat: api-routes / added the products routes

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,3 +1,74 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function Home() {
-  return <h1>hello world</h1>
+  return (
+    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          alt=""
+          width={920}
+          height={920}
+          quality={100}
+          src="/moletom-never-stop-learning.png"
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Never Stop Learning</span>
+
+          <span className="flex h-full items-center justify-center text-nowrap rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          alt=""
+          width={920}
+          height={920}
+          quality={100}
+          src="/moletom-java.png"
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Java</span>
+
+          <span className="flex h-full items-center justify-center text-nowrap rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          alt=""
+          width={920}
+          height={920}
+          quality={100}
+          src="/camisa-do-while.png"
+          className="transition-transform duration-500 group-hover:scale-105"
+        />
+
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Camiseta Do While</span>
+
+          <span className="flex h-full items-center justify-center text-nowrap rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
---
+++ src/data/api.ts
@@ -0,0 +1,10 @@
+import { RequestInit } from 'next/dist/server/web/spec-extension/request'
+
+import { env } from '~/env'
+
+export function api(path: string, init?: RequestInit) {
+  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const url = new URL(path, baseUrl)
+
+  return fetch(url, init)
+}
